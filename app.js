27b29d09163e66f07f2037d32e6693bb11d7a34b
Uygar Eren <komifb90@hotmail.com>
2023-04-21 14:17:39 +0300

    Views are done[C

---
+++ app.js
@@ -0,0 +1,9 @@
+const input = document.getElementById("search-input");
+const searchbtn = document.getElementById("searchBtn");
+const image = document.getElementById("image");
+const celcius = document.getElementById("celcius");
+const location = document.getElementById("location");
+const humidity = document.getElementById("humidity");
+const wind = document.getElementById("wind");
+
+
